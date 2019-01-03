1a9d8852e611544feab49d2d884bbc2180d37a69
jes3rk <joseph.schefer@gmail.com>
2019-01-03 17:58:16 -0500

    Added play and increment on spacebar

--- src/classes/Workspace.java
+++ src/classes/Workspace.java
@@ -1,4 +1,6 @@
 package classes;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
@@ -14,6 +16,18 @@ public class Workspace {
 	public Workspace(int w, int h) {
 		// first make the workspace
 		space.setLayout(new BoxLayout(space, BoxLayout.Y_AXIS));
+		space.setFocusable(true);
+		space.addKeyListener(new go());
+	}
+	
+	public void playCue() {
+		Cue goCue = cueList.get(itr);
+		goCue.go();
+		nextCue();
+	}
+	
+	public void nextCue() {
+		itr++;
 	}
 	
 	public void addAudioCue(String path, String name) {
@@ -31,5 +45,28 @@ public class Workspace {
 		}
 		
 		return space;
+	}
+	
+	public class go implements KeyListener {
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			int id = e.getID();
+			//System.out.println(id);
+			if (id == 401) {
+				playCue();
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 	}
 }
